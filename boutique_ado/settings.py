29d7de2cfea9749cf897749bf7b26e7628842b68
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-03-19 16:25:57 +0000

    ReUpdated SECRET_KEY & set DEBUG

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['roibeard-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://roibeard-boutique-ado.herokuapp.com/', 'localhost']
 
 
 # Application definition
