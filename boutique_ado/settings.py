aa189c6a2c749b9c2a55504a7f7a8d1a9d609c16
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-03-19 16:06:56 +0000

    Edit setting to reolve

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['roibeard-boutique-ado.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['roibeard-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
